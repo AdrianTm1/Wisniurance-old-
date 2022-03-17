c6541b808b6eaa10b063a6e4bde8af8fa5f915a8
AdrianTm1 <awojciechowski1@tm1.edu.pl>
2022-03-17 09:21:53 +0100

    v0.2.1

--- src/com/company/classes/AddressInforamation.java
+++ src/com/company/classes/AddressInforamation.java
@@ -6,4 +6,45 @@ public class AddressInforamation {
     private Integer flatNumber;
     private String zipCode;
 
+    public AddressInforamation() {
+    }
+
+    public AddressInforamation(String streetName, Integer streetNumber, Integer flatNumber, String zipCode) {
+        this.streetName = streetName;
+        this.streetNumber = streetNumber;
+        this.flatNumber = flatNumber;
+        this.zipCode = zipCode;
+    }
+
+    public String getStreetName() {
+        return streetName;
+    }
+
+    public void setStreetName(String streetName) {
+        this.streetName = streetName;
+    }
+
+    public Integer getStreetNumber() {
+        return streetNumber;
+    }
+
+    public void setStreetNumber(Integer streetNumber) {
+        this.streetNumber = streetNumber;
+    }
+
+    public Integer getFlatNumber() {
+        return flatNumber;
+    }
+
+    public void setFlatNumber(Integer flatNumber) {
+        this.flatNumber = flatNumber;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
 }
